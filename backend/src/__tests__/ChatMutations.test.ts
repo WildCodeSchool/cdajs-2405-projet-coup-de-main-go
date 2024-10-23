7b35489958f281f3c8e772178f8866660e0bd0a2
Sepulveda <guillaume.sepulveda@dixea.com>
2024-10-23 17:33:20 +0200

    tests mutation

---
+++ backend/src/__tests__/ChatMutations.test.ts
@@ -0,0 +1,138 @@
+import { ChatInput, ChatMutations } from "../resolvers/ChatMutations";
+import { MockTypeORM } from "mock-typeorm";
+import { faker } from "@faker-js/faker";
+import { User } from "../entities/User";
+import { Chat } from "../entities/Chat";
+import { Ad } from "../entities/Ad";
+import { Skill } from "../entities/Skill";
+
+describe("createChat", () => {
+  let chatMutations: ChatMutations;
+  let typeorm: MockTypeORM;
+  let userRequester: User;
+  let userHelper: User;
+  let ad: Ad;
+  let skill: Skill;
+  let chatData: ChatInput;
+
+  beforeAll(() => {
+    typeorm = new MockTypeORM();
+  });
+
+  beforeEach(() => {
+    chatMutations = new ChatMutations();
+
+    userRequester = new User(
+      faker.person.firstName(),
+      faker.person.lastName(),
+      faker.internet.email(),
+      faker.internet.password(),
+      faker.image.avatar(),
+      faker.location.streetAddress(),
+      faker.location.zipCode(),
+      faker.location.city()
+    );
+    userRequester.id = faker.string.uuid();
+
+    userHelper = new User(
+      faker.person.firstName(),
+      faker.person.lastName(),
+      faker.internet.email(),
+      faker.internet.password(),
+      faker.image.avatar(),
+      faker.location.streetAddress(),
+      faker.location.zipCode(),
+      faker.location.city()
+    );
+    userHelper.id = faker.string.uuid();
+
+    skill = new Skill(faker.lorem.word(), faker.image.url());
+
+    ad = new Ad(
+      faker.lorem.word(),
+      faker.lorem.sentence(),
+      faker.location.streetAddress(),
+      faker.location.zipCode(),
+      faker.location.city(),
+      faker.number.int(),
+      faker.number.int(),
+      userRequester,
+      skill
+    );
+    ad.id = faker.string.uuid();
+
+    chatData = {
+      adId: ad.id!,
+      userRequesterId: userRequester.id!,
+      userHelperId: userHelper.id!,
+    };
+  });
+
+  afterEach(() => {
+    typeorm.resetAll();
+  });
+
+  it("should successfully create a chat", async () => {
+    typeorm.onMock(User).toReturn(userRequester, "findOne");
+    typeorm.onMock(User).toReturn(userHelper, "findOne");
+    typeorm.onMock(Ad).toReturn(ad, "findOne");
+    typeorm.onMock(Chat).toReturn(null, "findOne");
+
+    const chat = await chatMutations.createChat(chatData);
+    expect(chat).toBeInstanceOf(Chat);
+    expect(chat.userRequester.id).toBe(userRequester.id);
+    expect(chat.userHelper.id).toBe(userHelper.id);
+  });
+
+  it("should throw an error if requester and helper are the same", async () => {
+    const chatData: ChatInput = {
+      adId: ad.id!,
+      userRequesterId: userRequester.id!,
+      userHelperId: userRequester.id!,
+    };
+
+    await expect(chatMutations.createChat(chatData)).rejects.toThrow(
+      "Le requester et le helper ne peuvent pas être la même personne."
+    );
+  });
+
+  it("should throw an error if requester does not exist", async () => {
+    typeorm.onMock(User).toReturn(null, "findOne");
+
+    await expect(chatMutations.createChat(chatData)).rejects.toThrow(
+      "L'utilisateur requester spécifié n'existe pas."
+    );
+  });
+
+  //   it("should throw an error if helper does not exist", async () => {
+  //     typeorm.onMock(User).toReturn(userRequester, "findOne");
+  //     typeorm.onMock(User).toReturn(null, "findOne");
+
+  //     await expect(chatMutations.createChat(chatData)).rejects.toThrow(
+  //       "L'utilisateur helper spécifié n'existe pas."
+  //     );
+  //   });
+
+  it("should throw an error if the ad does not exist or does not belong to the requester", async () => {
+    typeorm.onMock(User).toReturn(userRequester, "findOne");
+    typeorm.onMock(User).toReturn(userHelper, "findOne");
+    typeorm.onMock(Ad).toReturn(null, "findOne");
+
+    await expect(chatMutations.createChat(chatData)).rejects.toThrow(
+      "L'annonce spécifiée n'existe pas ou n'appartient pas à l'utilisateur requester."
+    );
+  });
+
+  it("should throw an error if a chat already exists", async () => {
+    typeorm.onMock(User).toReturn(userRequester, "findOne");
+    typeorm.onMock(User).toReturn(userHelper, "findOne");
+    typeorm.onMock(Ad).toReturn(ad, "findOne");
+    typeorm
+      .onMock(Chat)
+      .toReturn(new Chat(userRequester, userHelper, ad), "findOne");
+
+    await expect(chatMutations.createChat(chatData)).rejects.toThrow(
+      "Un chat existe déjà pour cette annonce entre ces deux utilisateurs."
+    );
+  });
+});
--- backend/src/__tests__/ChatQueries.test.ts
+++ backend/src/__tests__/ChatQueries.test.ts
@@ -9,7 +9,8 @@ import { Ad } from "../entities/Ad";
 describe("getChatsByUserId", () => {
   let chatQueries: ChatQueries;
   let typeorm: MockTypeORM;
-  let user: User;
+  let userRequester: User;
+  let userHelper: User;
   let skill: Skill;
   let ad: Ad;
 
@@ -18,19 +19,7 @@ describe("getChatsByUserId", () => {
 
     chatQueries = new ChatQueries();
 
-    ad = new Ad(
-      faker.lorem.word(),
-      faker.lorem.sentence(),
-      faker.location.streetAddress(),
-      faker.location.zipCode(),
-      faker.location.city(),
-      faker.number.int(),
-      faker.number.int(),
-      user,
-      skill
-    );
-
-    user = new User(
+    userRequester = new User(
       faker.person.firstName(),
       faker.person.lastName(),
       faker.internet.email(),
@@ -40,20 +29,53 @@ describe("getChatsByUserId", () => {
       faker.location.zipCode(),
       faker.location.city()
     );
-    user.id = faker.string.uuid();
+    userRequester.id = faker.string.uuid();
+
+    userHelper = new User(
+      faker.person.firstName(),
+      faker.person.lastName(),
+      faker.internet.email(),
+      faker.internet.password(),
+      faker.image.avatar(),
+      faker.location.streetAddress(),
+      faker.location.zipCode(),
+      faker.location.city()
+    );
+    userHelper.id = faker.string.uuid();
 
     skill = new Skill(faker.lorem.word(), faker.image.url());
+
+    ad = new Ad(
+      faker.lorem.word(),
+      faker.lorem.sentence(),
+      faker.location.streetAddress(),
+      faker.location.zipCode(),
+      faker.location.city(),
+      faker.number.int(),
+      faker.number.int(),
+      userRequester,
+      skill
+    );
   });
 
   const createMockChats = (count: number): Chat[] => {
-    return Array.from({ length: count }, () => new Chat(user, user, ad));
+    return Array.from({ length: count }, () => new Chat(userRequester, userHelper, ad));
   };
 
-  it("should return two chats", async () => {
+  it("should return two chats for user requester", async () => {
     const mockChats = createMockChats(2);
     typeorm.onMock(Chat).toReturn(mockChats, "find");
 
-    const chatsRetrieved = await chatQueries.getChatsByUserId(user.id!);
+    const chatsRetrieved = await chatQueries.getChatsByUserId(userRequester.id!);
+
+    expect(chatsRetrieved).toHaveLength(2);
+  });
+
+  it("should return two chats for user helper", async () => {
+    const mockChats = createMockChats(2);
+    typeorm.onMock(Chat).toReturn(mockChats, "find");
+
+    const chatsRetrieved = await chatQueries.getChatsByUserId(userHelper.id!);
 
     expect(chatsRetrieved).toHaveLength(2);
   });
@@ -62,7 +84,7 @@ describe("getChatsByUserId", () => {
     const mockChats = createMockChats(2);
     typeorm.onMock(Chat).toReturn(mockChats, "find");
 
-    const chatsRetrieved = await chatQueries.getChatsByUserId(user.id!);
+    const chatsRetrieved = await chatQueries.getChatsByUserId(userRequester.id!);
 
     expect(chatsRetrieved).toEqual(mockChats);
   });
@@ -71,7 +93,7 @@ describe("getChatsByUserId", () => {
     typeorm.onMock(User).toReturn(null, "findOne");
 
     try {
-      await chatQueries.getChatsByUserId(user.id!);
+      await chatQueries.getChatsByUserId(userRequester.id!);
     } catch (error) {
       expect(error).toBeInstanceOf(Error);
       if (error instanceof Error) {
@@ -81,10 +103,10 @@ describe("getChatsByUserId", () => {
   });
 
   it("should return an empty array if the user has no chats", async () => {
-    typeorm.onMock(User).toReturn(user, "findOne");
+    typeorm.onMock(User).toReturn(userRequester, "findOne");
     typeorm.onMock(Chat).toReturn([], "find");
 
-    const chatsRetrieved = await chatQueries.getChatsByUserId(user.id!);
+    const chatsRetrieved = await chatQueries.getChatsByUserId(userRequester.id!);
 
     expect(chatsRetrieved).toEqual([]);
   });
