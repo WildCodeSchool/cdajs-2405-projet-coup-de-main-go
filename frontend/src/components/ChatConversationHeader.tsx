218a7623283af9f615f94be9e2782f94cf4651c8
Sepulveda <guillaume.sepulveda@dixea.com>
2024-11-28 13:12:27 +0100

    update formatted date

--- frontend/src/components/ChatConversationHeader.tsx
+++ frontend/src/components/ChatConversationHeader.tsx
@@ -1,6 +1,6 @@
 import { Box, Typography } from "@mui/material";
 import PersonIcon from "@mui/icons-material/Person";
-import { formatDistanceToNow } from "date-fns";
+import { format } from "date-fns";
 import { fr } from "date-fns/locale";
 
 export default function ChatConversationHeader({
@@ -10,7 +10,7 @@ export default function ChatConversationHeader({
 }) {
   const registrationDate = new Date(userRegistrationDate);
 
-  const relativeDate = formatDistanceToNow(registrationDate, { addSuffix: true, locale: fr });
+  const formattedDate = format(registrationDate, "MMMM yyyy", { locale: fr });
 
   return (
     <Box sx={{ p: 1, pl: 2, borderBottom: 3, borderColor: "divider" }}>
@@ -24,7 +24,7 @@ export default function ChatConversationHeader({
       <Box sx={{ display: "flex" }}>
         <PersonIcon />
         <Typography variant="subtitle2" color="text.secondary">
-          Membre depuis {relativeDate}
+          Membre depuis {formattedDate}
         </Typography>
       </Box>
     </Box>
