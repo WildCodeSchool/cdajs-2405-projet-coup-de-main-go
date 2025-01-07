bc9f45aac999e4852115d068ed90f26bed87d939
Paul-Lac <plnlacroix@gmail.com>
2025-01-07 16:58:50 +0100

    add getUserOverviewById in UserQueries

--- backend/src/resolvers/UserQueries.ts
+++ backend/src/resolvers/UserQueries.ts
@@ -1,12 +1,12 @@
 import * as argon2 from "argon2";
 import jwt from "jsonwebtoken";
 import {
-    Resolver,
-    Query,
-    Arg,
-    Authorized,
-    ObjectType,
-    Field,
+  Resolver,
+  Query,
+  Arg,
+  Authorized,
+  ObjectType,
+  Field,
 } from "type-graphql";
 
 import { dataSource } from "../datasource";
@@ -15,108 +15,150 @@ import passwordVerification from "../utils/passwordVerification";
 
 @ObjectType()
 class LoginResponse {
-    @Field()
-    token!: string;
+  @Field()
+  token!: string;
 
-    @Field()
-    userId!: string;
+  @Field()
+  userId!: string;
+}
+
+@ObjectType()
+class UserOverview {
+  @Field()
+  user!: User;
+
+  @Field()
+  reviewsAsHelperCount!: number;
+
+  @Field({ nullable: true })
+  averageRating?: number;
 }
 
 @Resolver(User)
 export class UserQueries {
-    @Authorized()
-    @Query(() => [User])
-    async getAllUsers(): Promise<User[]> {
-        return await dataSource.manager.find(User, { relations: ["skills"] });
+  @Authorized()
+  @Query(() => [User])
+  async getAllUsers(): Promise<User[]> {
+    return await dataSource.manager.find(User, { relations: ["skills"] });
+  }
+
+  @Query(() => LoginResponse)
+  async login(
+    @Arg("email") email: string,
+    @Arg("password") password: string
+  ): Promise<LoginResponse> {
+    const user: User | null = await dataSource.manager.findOne(User, {
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error("Identifiants incorrects");
     }
 
-    @Query(() => LoginResponse)
-    async login(
-        @Arg("email") email: string,
-        @Arg("password") password: string
-    ): Promise<LoginResponse> {
-        const user: User | null = await dataSource.manager.findOne(User, {
-            where: { email },
-        });
+    // argon2
+    const isCorrectPassword: boolean = await argon2.verify(
+      user.password,
+      password
+    );
 
-        if (!user) {
-            throw new Error("Identifiants incorrects");
-        }
-
-        // argon2
-        const isCorrectPassword: boolean = await argon2.verify(
-            user.password,
-            password
-        );
-
-        if (!isCorrectPassword) {
-            throw new Error("Identifiants incorrects");
-        }
-
-        // jwt
-        const jwtSecret: string | undefined = process.env.JWT_SECRET;
-        if (!jwtSecret) {
-            throw new Error("invalid JWT secret");
-        }
-
-        const token: string = jwt.sign({ email, id: user.id }, jwtSecret, {
-            expiresIn: "24h",
-        });
-
-        return {
-            token,
-            userId: user.id!,
-        };
+    if (!isCorrectPassword) {
+      throw new Error("Identifiants incorrects");
     }
 
-    @Query(() => User)
-    async getUserByEmail(@Arg("email") email: string): Promise<User> {
-        const user: User | null = await dataSource.manager.findOne(User, {
-            relations: ["skills"],
-            where: { email },
-        });
-
-        if (!user) {
-            throw new Error("L'utilisateur n'existe pas");
-        }
-
-        return user;
+    // jwt
+    const jwtSecret: string | undefined = process.env.JWT_SECRET;
+    if (!jwtSecret) {
+      throw new Error("invalid JWT secret");
     }
 
-    @Query(() => Number)
-    async getMangoBalanceByUserId(@Arg("id") id: string): Promise<number> {
-        const user: User | null = await dataSource.manager.findOne(User, {
-            select: { mangoBalance: true },
-            where: { id },
-        });
+    const token: string = jwt.sign({ email, id: user.id }, jwtSecret, {
+      expiresIn: "24h",
+    });
 
-        if (!user) {
-            throw new Error("L'utilisateur n'existe pas");
-        }
+    return {
+      token,
+      userId: user.id!,
+    };
+  }
 
-        return user.mangoBalance;
+  @Query(() => User)
+  async getUserByEmail(@Arg("email") email: string): Promise<User> {
+    const user: User | null = await dataSource.manager.findOne(User, {
+      relations: ["skills"],
+      where: { email },
+    });
+
+    if (!user) {
+      throw new Error("L'utilisateur n'existe pas");
     }
 
-    @Query(() => Boolean)
-    async credentialsVerification(
-        @Arg("email") email: string,
-        @Arg("password") password: string,
-        @Arg("passwordConfirmation") passwordConfirmation: string
-    ): Promise<Boolean> {
-        const user: User | null = await dataSource.manager.findOne(User, {
-            where: { email },
-        });
+    return user;
+  }
 
-        if (user) {
-            throw new Error("L'adresse mail est déjà utilisée");
-        }
+  @Query(() => Number)
+  async getMangoBalanceByUserId(@Arg("id") id: string): Promise<number> {
+    const user: User | null = await dataSource.manager.findOne(User, {
+      select: { mangoBalance: true },
+      where: { id },
+    });
 
-        if (password !== passwordConfirmation) {
-            throw new Error("Les mots de passe doivent être identiques");
-        }
-
-        passwordVerification(password);
-
-        return true;
+    if (!user) {
+      throw new Error("L'utilisateur n'existe pas");
     }
+
+    return user.mangoBalance;
+  }
+
+  @Query(() => Boolean)
+  async credentialsVerification(
+    @Arg("email") email: string,
+    @Arg("password") password: string,
+    @Arg("passwordConfirmation") passwordConfirmation: string
+  ): Promise<Boolean> {
+    const user: User | null = await dataSource.manager.findOne(User, {
+      where: { email },
+    });
+
+    if (user) {
+      throw new Error("L'adresse mail est déjà utilisée");
+    }
+
+    if (password !== passwordConfirmation) {
+      throw new Error("Les mots de passe doivent être identiques");
+    }
+
+    passwordVerification(password);
+
+    return true;
+  }
+
+  @Query(() => UserOverview)
+  async getUserOverviewById(@Arg("id") id: string): Promise<UserOverview> {
+    const user = await dataSource.manager.findOne(User, {
+      where: { id },
+      relations: ["reviewsAsHelper"],
+    });
+
+    if (!user) {
+      throw new Error("L'utilisateur n'existe pas");
+    }
+
+    const reviewsAsHelper = (await user.reviewsAsHelper) || [];
+
+    const reviewsAsHelperCount = reviewsAsHelper.length;
+
+    const averageRating =
+      reviewsAsHelperCount > 0
+        ? (
+            reviewsAsHelper.reduce((acc, review) => acc + review.rating, 0) /
+            reviewsAsHelperCount
+          ).toFixed(1)
+        : null;
+
+    return {
+      user: user,
+      reviewsAsHelperCount,
+      averageRating: averageRating ? parseFloat(averageRating) : undefined,
+    };
+  }
 }
