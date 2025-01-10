423920e956a698e54632a10837c6a59bcb7bb185
Mattéo <matteo.donatelli1@gmail.com>
2025-01-10 11:29:34 +0100

    adding mobile version of auth modal

--- frontend/src/components/AuthModal/AuthModal.tsx
+++ frontend/src/components/AuthModal/AuthModal.tsx
@@ -4,30 +4,54 @@ import { useState } from "react";
 import Login from "./components/Login";
 import Register from "./components/Register";
 
-import "./AuthModal.css";
-
 function AuthModal() {
     const theme = useTheme();
     const isMobile = useMediaQuery(theme.breakpoints.down("sm"));
-    const [alreadyHasAnAccount, setAlreadyHasAnAccount] = useState<boolean>(true);
+    const [alreadyHasAnAccount, setAlreadyHasAnAccount] =
+        useState<boolean>(true);
     const [justRegistered, setJustRegistered] = useState<boolean>(false);
+    let modalStyles = {};
+    let image = "";
+    let imgStyles = {};
+
+    if (isMobile) {
+        modalStyles = {
+            height: "calc(100vh - 32px)",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+        };
+        image = "coup_de_main_go";
+        imgStyles = {
+            width: "128px",
+            borderRadius: "20px",
+        };
+    } else {
+        modalStyles = {
+            height: "600px",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "center",
+        };
+        image = "auth-modal-img";
+        imgStyles = {
+            width: "40%",
+            minHeight: "100%",
+            objectFit: "cover",
+            objectPosition: "40%",
+            borderRadius: "20px",
+        };
+    }
 
     return (
-        <Stack direction={"row"} height={"600px"}>
-            {!isMobile && (
-                <Box
-                    component="img"
-                    id="auth-modal-img"
-                    src="/images/auth-modal-img.png"
-                    sx={{
-                        width: "40%",
-                        minHeight: "100%",
-                        objectFit: "cover",
-                        objectPosition: "40%",
-                        borderRadius: "20px",
-                    }}
-                />
-            )}
+        <Stack sx={modalStyles}>
+            <Box
+                component="img"
+                id="auth-modal-img"
+                src={`/images/${image}.png`}
+                sx={imgStyles}
+            />
             {alreadyHasAnAccount ? (
                 <Login
                     justRegistered={justRegistered}
