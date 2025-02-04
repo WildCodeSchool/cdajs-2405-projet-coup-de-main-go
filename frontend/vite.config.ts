4d9201ba1f9575f33493daa9cea2b7fd34550928
Sepulveda <guillaume.sepulveda@dixea.com>
2025-02-04 17:14:32 +0100

    setup vitest

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -3,6 +3,11 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
     plugins: [react()],
+    test: {
+        globals : true , 
+        environment : "jsdom", 
+        setupFiles : "./src/tests/setup.ts"
+    },
     resolve: {
         alias: [
             { find: "@public", replacement: "/public" },
