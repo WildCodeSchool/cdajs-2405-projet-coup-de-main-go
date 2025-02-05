f91d63d72a89d08bd1aa4076e51bcc614ae390e8
Sepulveda <guillaume.sepulveda@dixea.com>
2025-02-05 11:42:40 +0100

    add playwright, user test, login user e2e and config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -2,20 +2,21 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-    plugins: [react()],
-    test: {
-        globals : true , 
-        environment : "jsdom", 
-        setupFiles : "./src/__tests__/setup.ts",
-        coverage: {
-            provider: 'istanbul',
-            reporter: ['text', 'json', 'html'],
-          },
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./src/__tests__/setup.ts",
+    coverage: {
+      provider: "istanbul",
+      reporter: ["text", "json", "html"],
     },
-    resolve: {
-        alias: [
-            { find: "@public", replacement: "/public" },
-            { find: "@components", replacement: "/src/components" },
-        ],
-    },
+    exclude: ["**/e2e/**"],
+  },
+  resolve: {
+    alias: [
+      { find: "@public", replacement: "/public" },
+      { find: "@components", replacement: "/src/components" },
+    ],
+  },
 });
