8ebe360c6f7976dcfe2c822f67eea75669b103d9
Sepulveda <guillaume.sepulveda@dixea.com>
2025-03-04 11:48:23 +0100

    update

--- backend/magidoc.mjs
+++ backend/magidoc.mjs
@@ -6,6 +6,7 @@ export default {
   website: {
     template: "carbon-multi-page",
     options: {
+      siteRoot: '/WildCodeSchool/cdajs-2405-projet-coup-de-main-go',
       queryGenerationFactories: {
         DateTimeISO: '"2025-03-04T12:00:00Z"'
       },
