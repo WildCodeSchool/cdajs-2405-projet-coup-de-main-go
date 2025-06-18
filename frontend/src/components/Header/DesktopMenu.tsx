1a2b75d6fa92d5e7a12e4b71a53fb9188a46fd46
Mattéo <matteo.donatelli1@gmail.com>
2025-06-18 16:03:05 +0200

    fix avatar after update

--- frontend/src/components/Header/DesktopMenu.tsx
+++ frontend/src/components/Header/DesktopMenu.tsx
@@ -4,55 +4,67 @@ import { useState } from "react";
 import { ExpandLess, ExpandMore } from "@mui/icons-material";
 
 interface DesktopMenuProps {
-  onLogout: () => void;
-  displayName: string;
-  loading: boolean;
-  error?: Error;
+    onLogout: () => void;
+    displayName: string;
+    loading: boolean;
+    error?: Error;
+    profilePicture?: string | null;
+    userId?: string;
 }
 
 export default function DesktopMenu({
-  onLogout,
-  displayName,
-  loading,
-  error,
+    onLogout,
+    displayName,
+    loading,
+    error,
+    profilePicture,
+    userId,
 }: DesktopMenuProps) {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const isMenuOpen = Boolean(anchorEl);
+    const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+    const isMenuOpen = Boolean(anchorEl);
 
-  const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
+    const handleProfileClick = (event: React.MouseEvent<HTMLElement>) => {
+        setAnchorEl(event.currentTarget);
+    };
 
-  const handleMenuClose = () => {
-    setAnchorEl(null);
-  };
+    const handleMenuClose = () => {
+        setAnchorEl(null);
+    };
 
-  return (
-    <>
-      {error ? (
-        <Typography color="error">Erreur: {error.message}</Typography>
-      ) : (
-        <Button
-          color="primary"
-          startIcon={
-            loading ? (
-              <CircularProgress size={24} color="secondary" />
+    return (
+        <>
+            {error ? (
+                <Typography color="error">Erreur: {error.message}</Typography>
             ) : (
-              <Avatar sx={{ width: 24, height: 24 }} />
-            )
-          }
-          endIcon={!loading && (isMenuOpen ? <ExpandLess /> : <ExpandMore />)}
-          onClick={handleProfileClick}
-          sx={{ textTransform: "none", minWidth: 120 }}
-        >
-          {!loading && displayName}
-        </Button>
-      )}
-      <ProfileMenu
-        anchorEl={anchorEl}
-        onClose={handleMenuClose}
-        onLogout={onLogout}
-      />
-    </>
-  );
+                <Button
+                    color="primary"
+                    startIcon={
+                        loading ? (
+                            <CircularProgress size={24} color="secondary" />
+                        ) : (
+                            <Avatar
+                                src={`${
+                                    import.meta.env.VITE_DOMAIN_BACKEND_URL
+                                }/uploads/users/${userId}/${profilePicture}`}
+                                sx={{ width: 24, height: 24 }}
+                            />
+                        )
+                    }
+                    endIcon={
+                        !loading &&
+                        (isMenuOpen ? <ExpandLess /> : <ExpandMore />)
+                    }
+                    onClick={handleProfileClick}
+                    sx={{ textTransform: "none", minWidth: 120 }}
+                >
+                    {!loading && displayName}
+                </Button>
+            )}
+            <ProfileMenu
+                anchorEl={anchorEl}
+                onClose={handleMenuClose}
+                onLogout={onLogout}
+            />
+        </>
+    );
 }
